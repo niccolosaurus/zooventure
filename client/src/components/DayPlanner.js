aa1c848bf9d92fcb933c32d7079ee36911bffc31
CarlosCastillo123 <ccastillo0098476@gmail.com>
2022-05-14 14:48:31 -0700

    added Bootstrap

---
+++ client/src/components/DayPlanner.js
@@ -0,0 +1,14 @@
+import React from "react"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {Card, Row, Col} from 'react-bootstrap'
+
+function DayPlanner () {
+
+    return (
+        <div>
+
+        </div>
+    )
+
+}
+ export default DayPlanner
