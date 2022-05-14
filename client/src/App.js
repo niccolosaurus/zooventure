e7b963f4f9d8b4ffb1e21b9432da032215ea4e5b
Daniel Holland <danhol1989@gmail.com>
2022-05-14 12:55:30 -0700

    created base Login and Signup page

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
+import Header from 'components/Header'
+import Login from 'pages/Login'
+import Signup from 'pages/Signup'
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Header />
+      <Routes>
+        <Route 
+          path="/" 
+          element={<Home />}
+        />
+        <Route 
+          path="/login" 
+          element={<Login />}
+        />
+        <Route 
+          path="/signup" 
+          element={<Signup />}
+        />
+        </Routes>
+    </ApolloProvider>
   );
 }
 
