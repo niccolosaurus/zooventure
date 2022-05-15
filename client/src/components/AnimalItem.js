58a225fc5f4b46aa8f1731ec1c679b4ba3e4e831
CarlosCastillo123 <ccastillo0098476@gmail.com>
2022-05-15 12:13:36 -0700

    Updated Item Card

--- client/src/components/AnimalItem.js
+++ client/src/components/AnimalItem.js
@@ -4,7 +4,8 @@ import { Link } from "react-router-dom";
 import { useStoreContext } from "../../utils/GlobalState";
 import { ADD_ANIMAL, REMOVE_ANIMAL } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {Card} from 'react-bootstrap'
 function AnimalItem(item) {
   const [state, dispatch] = useStoreContext();
 
@@ -12,33 +13,45 @@ function AnimalItem(item) {
     image,
     name,
     _id,
+    Lat,
+    Lon,
+    coord,
+    description,
+    funFact
     
   } = item;
 
   const { list } = state
 
   const addToList = () => {
-    const itemInCart = list.find((animalItem) => animalItem._id === _id)
-   if(data) {
+    const itemInList = list.find((animalItem) => animalItem._id === _id)
+   if(!itemInList) {
       dispatch({
         type: ADD_ANIMAL,
         animal: { ...item, }
       });
-      idbPromise('animals', 'put', { ...item,  });
+      idbPromise('animals', 'put', { ...item  });
+   } else {
+       return console.error('Animal aready in databse');
    }
   }
 
   return (
-    <div className="card px-1 py-1">
+    <div className="card">
+        <Card>
       <Link to={`/animals/${_id}`}>
         <img
           alt={name}
           src={`//${image}`}
         />
-        <p>{name}</p>
+        <Card.Title>{name}</Card.Title>
+        <Card.Text>{description}</Card.Text>
+        <Card.Text>{funFact}</Card.Text>
       </Link>
     
       <button onClick={addToList}>Add to Your list</button>
+
+      </Card>
     </div>
   );
 }
