2fe866aa1e003094f67ad1a2ca81c34918e37006
Niccolo Eck <nicco.eck@gmail.com>
2022-05-15 19:58:43 -0700

    updated code

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,20 +1,19 @@
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import ZooMap from "../components/ZooMap";
 
-var animalData = require()
+// var animalData = require();
 
 const Home = () => {
-
-  const [animal, setAnimal] = useState(animalData);
+  // const [animal, setAnimal] = useState(animalData);
 
   return (
     <div className="card bg-white card-rounded w-50">
-      <div className='image-container'>
-        <img src={`${process.env.PUBLIC_URL}/assets/images/ZooMap.jpg`} alt="Map of the Zoo"></img>
+      <div className="image-container">
+        {/* <img src={`${process.env.PUBLIC_URL}/assets/images/ZooMap.jpg`} alt="Map of the Zoo"></img> */}
+        <ZooMap />
       </div>
-      <div className='animal_list'>
-      
-      </div>
+      <div className="animal_list"></div>
     </div>
   );
 };
