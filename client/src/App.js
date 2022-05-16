ff343cd3e64c0680fd0bff0583811ecfbedb480e
Niccolo Eck <nicco.eck@gmail.com>
2022-05-15 19:59:22 -0700

    added the Home and commented out other code

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,31 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Header from './components/Header';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Home from './pages/Home';
+import Header from "./components/Header";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
 
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('id_token');
+  const token = localStorage.getItem("id_token");
   // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
@@ -37,23 +37,24 @@ const client = new ApolloClient({
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-      <Header />
-      <Routes>
-        <Route 
-          path="/" 
-          element={<Home />}
-        />
-        <Route 
-          path="/login" 
-          element={<Login />}
-        />
-        <Route 
-          path="/signup" 
-          element={<Signup />}
-        />
-        </Routes>
-    </ApolloProvider>
+    <Home />
+    // <ApolloProvider client={client}>
+    //   <Header />
+    //   <Routes>
+    //     <Route
+    //       path="/"
+    //       element={<Home />}
+    //     />
+    //     <Route
+    //       path="/login"
+    //       element={<Login />}
+    //     />
+    //     <Route
+    //       path="/signup"
+    //       element={<Signup />}
+    //     />
+    //     </Routes>
+    // </ApolloProvider>
   );
 }
 
