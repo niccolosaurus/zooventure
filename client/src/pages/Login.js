5ff5607ec687460d5c798d437a7f929b41dec798
CarlosCastillo123 <ccastillo0098476@gmail.com>
2022-05-19 16:19:01 -0700

    updated login with bootstrap

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
-
+import background from "../paws.jpeg"
 import Auth from '../utils/auth';
 
 const Login = (props) => {
@@ -41,42 +41,58 @@ const Login = (props) => {
   };
 
   return (
-    <main className="flex-row justify-center mb-4">
-      <div className="col-12 col-lg-10">
-        <div className="card">
-          <h4 className="card-header bg-dark text-light p-2">Login</h4>
-          <div className="card-body">
+    <main className="flex-row justify-center">
+      <div>
+        <div className="card" style={{height:"100vh" , display: "flex", alignItems: "center", justifyContent: 'start', backgroundImage: `url(${background})` }} >
+          {/* <h4 style={{textAlign:'center', marginTop: "10px", marginBottom: '10px' ,borderRadius:"10px", color:"white"}}>Login to make a plan or <a href='/signup'>signup</a></h4> */}
+        \
             {data ? (
+                <div className="card-body">
               <p>
                 Success! You may now head{' '}
                 <Link to="/">back to the homepage.</Link>
               </p>
+              </div>
             ) : (
-              <form onSubmit={handleFormSubmit}>
-                <input
-                  className="form-input"
-                  placeholder="Your email"
-                  name="email"
-                  type="email"
-                  value={formState.email}
-                  onChange={handleChange}
-                />
-                <input
-                  className="form-input"
-                  placeholder="******"
-                  name="password"
-                  type="password"
-                  value={formState.password}
-                  onChange={handleChange}
-                />
-                <button
-                  className="btn btn-block btn-info"
-                  style={{ cursor: 'pointer' }}
-                  type="submit"
-                >
-                  Submit
-                </button>
-              </form>
+              <div className='card' style={{ display: "flex",marginTop: "60px", alignItems: "center", justifyContent: 'center', backgroundColor: 'rgba(52, 52, 52, 0.8)', borderRadius: "10px" }} >
+
+                <form onSubmit={handleFormSubmit}>
+
+                  <div className='card-header'>
+                    <h4 style={{ textAlign: 'center', marginTop: "10px", marginBottom: '10px', borderRadius: "10px", color: "white" }}>Welcome back! Login to make a plan or <a href='/signup'>signup</a></h4>
+                  </div>
+                  <div style={{ margin: '10px' }} className='mb-3 from-group'>
+                    <label style={{ color: 'white' }} for="email" class="form-label"><strong>Email address</strong></label>
+                    <input
+                      className="form-control"
+                      placeholder="Your email"
+                      name="email"
+                      type="email"
+                      value={formState.email}
+                      onChange={handleChange}
+                    />
+                  </div>
+                  <div style={{ margin: '10px' }} className='mb-3 form-group'>
+                    <label style={{ color: 'white' }} for="password" class="form-label"><strong>Password</strong></label>
+                    <input
+                      className="form-control"
+                      placeholder="******"
+                      name="password"
+                      type="password"
+                      value={formState.password}
+                      onChange={handleChange}
+                    />
+                  </div>
+                  <button
+                    className="btn btn-block btn-primary"
+                    style={{ cursor: 'pointer', margin: '10px' }}
+                    type="submit"
+                  >
+                    <strong>Submit</strong>
+                  </button>
+
+                </form>
+              </div>
             )}
 
             {error && (
@@ -84,7 +100,7 @@ const Login = (props) => {
                 {error.message}
               </div>
             )}
-          </div>
+         
         </div>
       </div>
     </main>
