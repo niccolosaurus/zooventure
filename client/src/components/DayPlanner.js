f9e8750ec1e6070d33b6cfa93e6a5ba6deed8900
CarlosCastillo123 <ccastillo0098476@gmail.com>
2022-05-19 22:04:20 -0700

    Added basic list

--- client/src/components/DayPlanner.js
+++ client/src/components/DayPlanner.js
@@ -1,12 +1,29 @@
 import React from "react"
 import 'bootstrap/dist/css/bootstrap.min.css'
-import {Card, Row, Col} from 'react-bootstrap'
+import {Card, Row, Col, ListGroup,} from 'react-bootstrap'
+import { Data } from "@react-google-maps/api"
+import { QUERY_ANIMALS, QUERY_USERS } from "../utils/queries";
+import {useQuery} from '@apollo/client'
+import {QUERY_USER} from "../utils/queries"
 
 function DayPlanner () {
+    const { data, loading, error } = useQuery(QUERY_ANIMALS);
 
+    if (loading) return "Loading...";
+    if (error) return <pre>{error.message}</pre>
     return (
-        <div>
+        <div style={{width: "300px"}}>
+           <ListGroup >
+           {data.users.plans.animals.map((animal) => (
+                     <Card style={{backgroundColor: " rgb(14, 99, 41, .8)"}}>
+                         
+                         <Card.Header style={{textAlign: 'center'}}>{animal.name}</Card.Header>
+                         
+                      
+                     </Card>
+               ))}
 
+     </ListGroup>
         </div>
     )
 
