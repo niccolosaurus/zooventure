b2cc032e4adf66ca9c88c6d2ef7a6816faabd073
Daniel Holland <danhol1989@gmail.com>
2022-05-21 11:30:45 -0700

    Product model & seeds created

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,5 @@
 const User = require('./User');
 const Animal = require('./Animal');
-const Plan = require('./Plan')
-module.exports = { User, Animal, Plan };
+const Plan = require('./Plan');
+const Product = require('./Product');
+module.exports = { User, Animal, Plan, Product };
