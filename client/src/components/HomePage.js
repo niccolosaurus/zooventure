b98364298415cb85cee4c54438d2bdbab777b786
Niccolo Eck <nicco.eck@gmail.com>
2022-05-21 14:13:43 -0700

    styling the HomePage.js

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -4,46 +4,50 @@ import { Card, Row, Col, Accordion, Tab, Tabs, Button } from "react-bootstrap";
 
 function HomePage() {
   return (
-    <div>
-      <div>
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
+    <div
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "center",
+        alignContent: "center",
+        margin: "20px",
+      }}
+    >
+      <div style={{ margin: "20px" }}>
+        <Card style={{ width: "40rem", height: "40rem" }}>
+          <Card.Img
+            variant="top"
+            src={process.env.PUBLIC_URL + "/assets/images/ZooMap.jpg"}
+          />
           <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              View our zoo map.
-            </Card.Text>
+            <Card.Title>Zooventure Map</Card.Title>
+            <Card.Text>View our zoo map.</Card.Text>
             <Button variant="primary">Click for Map</Button>
           </Card.Body>
         </Card>
       </div>
 
-      <div>
-        <Card style={{ width: "18rem" }}>
+      <div style={{ margin: "20px" }}>
+        <Card style={{ width: "40rem", height: "40rem" }}>
           <Card.Img variant="top" src="holder.js/100px180" />
           <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              View our zoo map.
-            </Card.Text>
+            <Card.Title>Zooventure Gift Shop</Card.Title>
+            <Card.Text>View our zoo map.</Card.Text>
             <Button variant="primary">Click for Map</Button>
           </Card.Body>
         </Card>
       </div>
 
-      <div>
-        <Card style={{ width: "30rem" }}>
+      <div style={{ margin: "20px" }}>
+        <Card style={{ width: "40rem", height: "40rem" }}>
           <Card.Img variant="top" src="holder.js/100px180" />
           <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              View our zoo map.
-            </Card.Text>
+            <Card.Title>Zooventure AR</Card.Title>
+            <Card.Text>View our zoo map.</Card.Text>
             <Button variant="primary">Click for Map</Button>
           </Card.Body>
         </Card>
       </div>
-
     </div>
   );
 }
