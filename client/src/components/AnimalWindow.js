28703c62704ef1e825bd48c31560f5ae04a13b8f
Daniel Holland <danhol1989@gmail.com>
2022-05-20 21:33:28 -0700

    added photos to AnimalWindow

--- client/src/components/AnimalWindow.js
+++ client/src/components/AnimalWindow.js
@@ -7,14 +7,17 @@ const divStyle = {
   padding: 15,
 };
 
-const AnimalWindow = ({ animal, onCloseClick }) => {
+// const AnimalWindow = ({ animal, onCloseClick }) => {
+const AnimalWindow = ({ animal }) => {
   const position = {
     lat: animal.Lat,
-    lng: animal.Lng,
+    lng: animal.Lon,
   };
   return (
-    <InfoWindow position={position} onCloseClick={onCloseClick}>
+    // <InfoWindow position={position} onCloseClick={onCloseClick}>
+    <InfoWindow position={position}>
       <div style={divStyle}>
+        <img src={animal.img} alt={animal.name}></img>
         <h1>{animal.name}</h1>
       </div>
     </InfoWindow>
