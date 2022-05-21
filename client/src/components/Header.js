fab4af17ecf8b3db3cefbf96e39733236784611f
Niccolo Eck <nicco.eck@gmail.com>
2022-05-21 13:43:58 -0700

    updated the Header.js for new routes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,69 +1,77 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import Auth from '../utils/auth';
+import Auth from "../utils/auth";
 
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar, Container, Nav } from 'react-bootstrap'; 
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar, Container, Nav } from "react-bootstrap";
 const Header = () => {
-
   const logout = (event) => {
     event.preventDefault();
     Auth.logout();
   };
 
   return (
-    
-    
-       <Navbar style={{width: "100%", backgroundColor: 'rgba(52, 52, 52, 0.8)' }}  expand="bg" >
-    <Container style={{width: "100%"}}>
-      <Navbar.Brand style={{color: "white"}} href="/"><strong>Zooventure</strong></Navbar.Brand>
-       
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
-          {Auth.loggedIn() ? (
-            <>
-       
-          
-              <Nav.Link style={{color: "white"}} className="nav-link" href="/me">
-                My Trip
-              </Nav.Link>
-              
-              <Nav.Link style={{color: "white"}} className="nav-link" to='/'  onClick={logout}>
-                Logout
-              </Nav.Link>
+    <Navbar
+      style={{ width: "100%", backgroundColor: "rgba(52, 52, 52, 0.8)" }}
+      expand="bg"
+    >
+      <Container style={{ width: "100%" }}>
+        <Navbar.Brand style={{ color: "white" }} href="/">
+          <strong>Zooventure</strong>
+        </Navbar.Brand>
 
-               
-             
-             
-            </>
-          ) : (
-            <>
-    
-            
-         
-              <Nav.Link style={{color: "white"}} className="nav-link active" href="/login">
-                Login
-              </Nav.Link>
-     
-              <Nav.Link style={{color: "white"}} className="nav-link active" href="/signup">
-                Signup
-              </Nav.Link>
-            
-           
-             
-            </>
-          )}
-          <Nav.Link style={{color: "white"}} className="nav-link active" href="/">
-                Home
-              </Nav.Link>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            {Auth.loggedIn() ? (
+              <>
+                <Nav.Link
+                  style={{ color: "white" }}
+                  className="nav-link"
+                  href="/profile"
+                >
+                  My Trip
+                </Nav.Link>
+
+                <Nav.Link
+                  style={{ color: "white" }}
+                  className="nav-link"
+                  to="/"
+                  onClick={logout}
+                >
+                  Logout
+                </Nav.Link>
+              </>
+            ) : (
+              <>
+                <Nav.Link
+                  style={{ color: "white" }}
+                  className="nav-link active"
+                  href="/login"
+                >
+                  Login
+                </Nav.Link>
+
+                <Nav.Link
+                  style={{ color: "white" }}
+                  className="nav-link active"
+                  href="/signup"
+                >
+                  Signup
+                </Nav.Link>
+              </>
+            )}
+            <Nav.Link
+              style={{ color: "white" }}
+              className="nav-link active"
+              href="/"
+            >
+              Home
+            </Nav.Link>
           </Nav>
-            </Navbar.Collapse>
-            </Container>
-        </Navbar>
-       
-  
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
