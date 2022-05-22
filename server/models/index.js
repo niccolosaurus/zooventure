f000b7f5adba2e494065e2f8c489267757c647c9
Daniel Holland <danhol1989@gmail.com>
2022-05-22 16:01:41 -0700

    purged store from code

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,5 @@
 const User = require('./User');
 const Animal = require('./Animal');
 const Plan = require('./Plan');
-const Product = require('./Product');
-const Order = require('./Order')
-module.exports = { User, Animal, Plan, Product, Order };
+
+module.exports = { User, Animal, Plan };
