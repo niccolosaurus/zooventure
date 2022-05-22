f65baaeb9b13115f79de123dff82e77f592d203d
CarlosCastillo123 <ccastillo0098476@gmail.com>
2022-05-21 20:37:06 -0700

    updated my animal list

--- client/src/components/MyAnimalList.js
+++ client/src/components/MyAnimalList.js
@@ -1,51 +1,66 @@
 import React, { useEffect } from 'react';
-
-import { idbPromise } from '../../utils/helpers';
-
-import { useStoreContext } from '../../utils/GlobalState';
-import { ADD_ANIMAL} from '../../utils/actions';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import AnimalItem from './AnimalItem';
+import { Card, Row, Col, Accordion, Tab, Tabs, Button } from 'react-bootstrap';
+const MyAnimalList = (input) => {
+ 
 
+  const {
+    _id,
+    description,
+    name,
+    funFact,
+    img,
 
-
-const AnimalList = () => {
-  const [state, dispatch] = useStoreContext();
-
-  useEffect(() => {
-    async function getList() {
-      const animal = await idbPromise('animals', 'get');
-      dispatch({ type: ADD_ANIMAL, products: [...animal] });
-    }
-
-    if (!state.animal.length) {
-      getList();
-    }
-  }, [state.animal.length, dispatch]);
+  } = input
 
   
   return (
     <div className="animalList">
-      
-      <h2>My animals</h2>
-      {state.animals.length ? (
-        <div>
-           
-          {state.animals.map((animal) => (
-            <AnimalItem key={animal._id} animal={animal} />
-          ))}
+    <Card key={_id} variant="success"  style={{ border: " 10px solid #563e01", backgroundColor: "#ebb905", width: "300px", margin: "20px" }}>
+        <img style={{ border: " 10px solid #edac02", alignContent: "left" }} src={img} />
+        <Card.Body>
+          <Card.Title style={{ fontSize: "25px", textDecoration: "underline", textAlign: "center" }}>
 
-        </div>
-      ) : (
-        <h3>
-          <span role="img" aria-label="shocked">
-            😱
-          </span>
-          You haven't added any Animals!
-        </h3>
-      )}
+            {name}
+          </Card.Title>
+          <hr></hr>
+
+          <Col>
+            <>
+
+              <Accordion variant="warning" style={{ backgroundColor: "#b58404" }} flush>
+                <Accordion.Item eventKey="0" style={{ backgroundColor: "#b58404" }}>
+                  <Accordion.Header variant="warning" style={{ backgroundColor: "#b58404" }}>Description</Accordion.Header>
+                  <Accordion.Body >
+                    {description}
+                  </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item style={{ backgroundColor: "#b58404" }} eventKey="1">
+                  <Accordion.Header style={{ backgroundColor: "#b58404" }}>Fun Facts</Accordion.Header>
+                  <Accordion.Body>
+                    {funFact}
+                  </Accordion.Body>
+                </Accordion.Item>
+              </Accordion>
+
+            </>
+          </Col>
+          <Col>
+            <>
+            </>
+          </Col>
+          <Col>
+
+          </Col>
+
+        </Card.Body>
+         
+         <Card.Footer style={{ padding: "0px",margin: "0px" , textAlign: "center" }}>
+           <Button style={{borderRadius: "0px"  ,height: "100%", width: "100%"}} href='' type='click' size="lg" variant="warning" id="add-plan">Remove Animal from plan</Button>
+         </Card.Footer> 
+      </Card>
     </div>
   );
 };
 
-export default AnimalList;
+export default MyAnimalList;
