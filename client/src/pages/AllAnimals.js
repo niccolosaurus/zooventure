8c9906173f81feec4b83e8e765ff1d8593d2e7a0
CarlosCastillo123 <ccastillo0098476@gmail.com>
2022-05-22 21:29:39 -0700

    made page to display animals

---
+++ client/src/pages/AllAnimals.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+import AnimalCard from "../components/AnimalCard"
+
+const AllAnimals = () => {
+    return ( 
+        <div>
+        <AnimalCard/>
+        </div>
+    )
+}
+ export default AllAnimals
