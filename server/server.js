e7dcdc39670454ccc139112929b6cc80bdcd8d94
Daniel Holland <danhol1989@gmail.com>
2022-05-23 16:34:55 -0700

    added catch all for server.js

--- server/server.js
+++ server/server.js
@@ -42,6 +42,7 @@ const startApolloServer = async (typeDefs, resolvers) => {
   })
   };
 
+  
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
   });
