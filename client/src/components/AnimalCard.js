97afc3da54046dcc1d97942f6f6810076e59a3d2
Niccolo Eck <nicco.eck@gmail.com>
2022-05-23 23:12:14 -0700

    You can now add animals from the all animals page to the profile page

--- client/src/components/AnimalCard.js
+++ client/src/components/AnimalCard.js
@@ -2,86 +2,90 @@ import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Card, Row, Col, Accordion, Tab, Tabs, Button } from "react-bootstrap";
 import logo from "../logo.svg";
-import background from "../paws.jpeg";
 import AccordionHeader from "react-bootstrap/esm/AccordionHeader";
 import "bootstrap/dist/css/bootstrap.css";
 import "./Accordion.css";
 import { QUERY_ANIMALS } from "../utils/queries";
-import { useQuery } from "@apollo/client";
+import { useMutation, useQuery } from "@apollo/client";
+import { ADD_PLAN } from "../utils/mutations";
 
-function AnimalCard() {
-  const { data, loading, error } = useQuery(QUERY_ANIMALS);
+function AnimalCard(props) {
+  const { _id, description, name, funFact, img, animal } = props;
+  const { data, loading, err } = useQuery(QUERY_ANIMALS);
+  const [addPlan] = useMutation(ADD_PLAN);
+
+  const revisedAnimal = {
+    _id: animal._id,
+    name: animal.name,
+    coord: animal.coord,
+    Lat: animal.Lat,
+    Lon: animal.Lon,
+    description: animal.description,
+    img: animal.img,
+
+    funFact: animal.funFact,
+  };
 
   if (loading) return "Loading...";
-  if (error) return <pre>{error.message}</pre>;
+  if (err) return <pre>{err.message}</pre>;
 
   return (
-    <div
-      className="Container"
-      style={{
-        backgroundImage: `url(${background})`,
-        display: "flex",
-        flexWrap: "wrap",
-        justifyContent: "center",
-        alignContent: "center",
-      }}
+    <Card
+      variant="success"
+      key={_id}
+      style={{ backgroundColor: "#b58404", width: "20rem", margin: "20px" }}
     >
-   
+      <Card.Img style={{ alignContent: "left" }} src={img} />
+      <Card.Body style={{ backgroundColor: "#fcdb03" }}>
+        <Card.Title>{name}</Card.Title>
 
-      {data.animals.map((animal) => (
-        <Card
-          variant="success"
-          key={animal._id}
-          style={{ backgroundColor: "#b58404", width: "20rem", margin: "20px" }}
-        >
-          <Card.Img style={{ alignContent: "left" }} src={animal.img} />
-          <Card.Body style={{backgroundColor: "#fcdb03"}}>
-            <Card.Title>{animal.name}</Card.Title>
-
-            <Col>
-              <>
-                <Accordion
+        <Col>
+          <>
+            <Accordion
+              variant="warning"
+              style={{ backgroundColor: "#b58404" }}
+              flush
+            >
+              <Accordion.Item
+                eventKey="0"
+                style={{ backgroundColor: "#b58404" }}
+              >
+                <AccordionHeader
                   variant="warning"
                   style={{ backgroundColor: "#b58404" }}
-                  flush
                 >
-                  <Accordion.Item
-                    eventKey="0"
-                    style={{ backgroundColor: "#b58404" }}
-                  >
-                    <AccordionHeader
-                      variant="warning"
-                      style={{ backgroundColor: "#b58404" }}
-                    >
-                      Description
-                    </AccordionHeader>
-                    <Accordion.Body>{animal.description}</Accordion.Body>
-                  </Accordion.Item>
-                  <Accordion.Item
-                    style={{ backgroundColor: "#b58404" }}
-                    eventKey="1"
-                  >
-                    <Accordion.Header style={{ backgroundColor: "#b58404" }}>
-                      Fun Facts
-                    </Accordion.Header>
-                    <Accordion.Body>{animal.funFact}</Accordion.Body>
-                  </Accordion.Item>
-                </Accordion>
-              </>
-            </Col>
-            <Col>
-              <></>
-            </Col>
-            <Col></Col>
-          </Card.Body>
-          <Card.Footer style={{ textAlign: "center" }}>
-            <Button size="lg" variant="warning" id="add-plan">
-              Add Animal to Plan
-            </Button>
-          </Card.Footer>
-        </Card>
-      ))}
-    </div>
+                  Description
+                </AccordionHeader>
+                <Accordion.Body>{description}</Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item
+                style={{ backgroundColor: "#b58404" }}
+                eventKey="1"
+              >
+                <Accordion.Header style={{ backgroundColor: "#b58404" }}>
+                  Fun Facts
+                </Accordion.Header>
+                <Accordion.Body>{funFact}</Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
+          </>
+        </Col>
+        <Col>
+          <></>
+        </Col>
+        <Col></Col>
+      </Card.Body>
+      <Card.Footer style={{ textAlign: "center" }}>
+        <Button
+          size="lg"
+          variant="warning"
+          id="add-plan"
+          onClick={() => addPlan({ variables: { animals: revisedAnimal._id } })}
+        >
+          Add Animal to Plan
+        </Button>
+      </Card.Footer>
+    </Card>
   );
 }
 export default AnimalCard;
