69916d5b82cd7c01d07e5c50a7d9580d07fa511c
Niccolo Eck <nicco.eck@gmail.com>
2022-05-23 19:19:56 -0700

    fixes

--- client/src/components/ZooMap.js
+++ client/src/components/ZooMap.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-// import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
-// import { Marker } from "@react-google-maps/api";
+import { useGoogleMap } from "@react-google-maps/api";
+import { Marker } from "@react-google-maps/api";
 
 import AnimalWindow from "./AnimalWindow";
 
@@ -19,87 +19,88 @@ const center = {
 
 function ZooMap() {
   const { loading, data, error } = useQuery(QUERY_ANIMALS);
+  const GoogleMap = useGoogleMap()
 
   // const { isLoaded } = useJsApiLoader({
   //   id: "google-map-script",
   //   googleMapsApiKey: "AIzaSyCuiil7MzZxeyMnEcO12VFm_VQdGaxYgDQ",
   // });
 
-  return <div> hello world </div>;
 
-  // const [map, setMap] = useState(null);
 
-  // const [showAnimalWindow, setShowAnimalWindow] = useState(false);
-  // const [singleAnimal, setSingleAnimal] = useState({});
+  const [map, setMap] = useState(null);
 
-  // const closeAnimalWindow = () => {
-  //   setShowAnimalWindow(false);
-  //   setSingleAnimal({});
-  // };
+  const [showAnimalWindow, setShowAnimalWindow] = useState(false);
+  const [singleAnimal, setSingleAnimal] = useState({});
 
-  // const onLoad = React.useCallback(function callback(map) {
-  //   setMap(map);
-  // }, []);
+  const closeAnimalWindow = () => {
+    setShowAnimalWindow(false);
+    setSingleAnimal({});
+  };
 
-  // const onUnmount = React.useCallback(function callback(map) {
-  //   setMap(null);
-  // }, []);
+  const onLoad = React.useCallback(function callback(map) {
+    setMap(map);
+  }, []);
 
-  // const getAnimalData = (animal) => {
-  //   console.log(animal);
-  //   setSingleAnimal(animal);
-  //   setShowAnimalWindow(true);
-  // };
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null);
+  }, []);
 
-  // if (loading) return "Loading..";
-  // if (error) return <pre>{error.message}</pre>;
+  const getAnimalData = (animal) => {
+    console.log(animal);
+    setSingleAnimal(animal);
+    setShowAnimalWindow(true);
+  };
 
-  // console.log(data);
-  // console.log({ showAnimalWindow });
+  if (loading) return "Loading..";
+  if (error) return <pre>{error.message}</pre>;
 
-  // if (data) {
-  //   return (
-  //     <GoogleMap
-  //       id="gmap-map"
-  //       mapContainerStyle={containerStyle}
-  //       center={center}
-  //       onLoad={onLoad}
-  //       onUnmount={onUnmount}
-  //       zoom={17}
-  //       options={{ mapId: "830da1546c37eea8" }}
-  //     >
-  //       {/* Child components, such as markers, info windows, etc. */}
-  //       <>
-  //         {data &&
-  //           data?.animals?.map((animal) => {
-  //             const position = {
-  //               lng: animal.Lon,
-  //               lat: animal.Lat,
-  //             };
-  //             console.log({ animal });
-  //             return (
-  //               <Marker
-  //                 // key={animal._id}
-  //                 key={Math.random()}
-  //                 position={position}
-  //                 onClick={() => getAnimalData(animal)}
-  //                 icon={process.env.PUBLIC_URL + "/assets/images/paw_icon.png"}
-  //               >
-  //                 {showAnimalWindow && singleAnimal.name === animal.name && (
-  //                   <AnimalWindow
-  //                     animal={singleAnimal}
-  //                     onCloseClick={closeAnimalWindow}
-  //                   />
-  //                 )}
-  //               </Marker>
-  //             );
-  //           })}
+  console.log(data);
+  console.log({ showAnimalWindow });
 
-  //         {/*  */}
-  //       </>
-  //     </GoogleMap>
-  //   );
-  // }
+  if (data) {
+    return (
+      <GoogleMap
+        id="gmap-map"
+        mapContainerStyle={containerStyle}
+        center={center}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+        zoom={17}
+        options={{ mapId: "830da1546c37eea8" }}
+      >
+        {/* Child components, such as markers, info windows, etc. */}
+        <>
+          {data &&
+            data?.animals?.map((animal) => {
+              const position = {
+                lng: animal.Lon,
+                lat: animal.Lat,
+              };
+              console.log({ animal });
+              return (
+                <Marker
+                  // key={animal._id}
+                  key={Math.random()}
+                  position={position}
+                  onClick={() => getAnimalData(animal)}
+                  icon={process.env.PUBLIC_URL + "/assets/images/paw_icon.png"}
+                >
+                  {showAnimalWindow && singleAnimal.name === animal.name && (
+                    <AnimalWindow
+                      animal={singleAnimal}
+                      onCloseClick={closeAnimalWindow}
+                    />
+                  )}
+                </Marker>
+              );
+            })}
+
+          {/*  */}
+        </>
+      </GoogleMap>
+    );
+  }
 }
 
 export default ZooMap;
